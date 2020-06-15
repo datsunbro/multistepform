14d61f867f3d0a72a88c70569d534b34ab0612b0
robert.sierl <robert.sierl@googlemail.com>
2020-06-15 12:41:07 +0200

    Implemented the request for lead processing and added additional validation. Also changed the theme colors to match the new product theme.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,21 +1,16 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-Vue.use(Vuetify);
-
 const vuetify = new Vuetify({
     theme: {
         themes: {
             light: {
-                primary: '#0090D6',
+                primary: '#005d70',
             }
         },
     },
 })
+Vue.use(Vuetify);
 
 export default vuetify;
 
-/*
-export default new Vuetify({
-});
- */
